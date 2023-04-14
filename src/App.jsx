bbf9a30a8f53f56f6cd93340454511f7c68ed5be
Hasan Kılıç <97hasankilic@gmail.com>
2023-04-14 04:39:27 +0300

    Form created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      
+    <div>
+    <h1>ToDo List</h1>
+    <form className = "new-item-form"  action="">
+      <div className='form-row'>
+        <label htmlFor="item">New Item</label>
+        <input type="text" id="item"/>
+      </div>
+      <button className='btn'>Add</button>
+    </form>
     </div>
   )
 }
