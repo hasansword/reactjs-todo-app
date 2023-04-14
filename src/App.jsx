bf8c502e89da996a671e64844b86aecee7389fce
Hasan Kılıç <97hasankilic@gmail.com>
2023-04-14 05:18:44 +0300

    coded for delete and completed todo function

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,25 @@ function App() {
         { id: crypto.randomUUID(), title: newItem, completed: false },
       ];
     });
+
+    setNewItem("");
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (id === todo.id) {
+          return { ...todo, completed };
+        }
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id){
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    })
   }
 
   return (
@@ -33,15 +52,24 @@ function App() {
 
       <h1 className="header">ToDo List</h1>
       <ul className="list">
+        {todos.length === 0 && "No Todo. Add your first todo."}
         {todos.map((todo) => (
           <li key={todo.id}>
-            {todo.title}
-            <button className="btn btn-danger" onClick={() => setTodos((currentTodos) => currentTodos.filter((t) => t.id!== todo.id))}>
+            <label>
+              {todo.title}
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+              />
+            </label>
+            <button
+              className="btn btn-danger"
+              onClick={() => deleteTodo(todo.id)}>
               Delete
             </button>
           </li>
         ))}
-        
       </ul>
     </>
   );
